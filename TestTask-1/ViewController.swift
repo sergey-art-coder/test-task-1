d2bb84497ccd313b4183b136aaa572f734676183
sergey-art-coder <ser.lyashenko@gmail.com>
2021-10-12 01:09:11 +0300

    Initialize project

---
+++ TestTask-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask-1
+//
+//  Created by Сергей Ляшенко on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
